0cdbf3716a7f1b01b855433973a46728efac146c
manabee99 <mab@ss.iij4u.or.jp>
2020-07-24 13:51:34 +0900

    Updates

---
+++ precache-manifest.ae9d4111573d2792d13cc7bdba2f5bfa.js
@@ -0,0 +1,190 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cec4a303ee32497d38b376a21393a2ab",
+    "url": "/kids-typing/index.html"
+  },
+  {
+    "revision": "ccd65896cfffc35565c3",
+    "url": "/kids-typing/static/css/main.3659710d.chunk.css"
+  },
+  {
+    "revision": "8c4f00869857b1d9f37b",
+    "url": "/kids-typing/static/js/2.eeb8d9ce.chunk.js"
+  },
+  {
+    "revision": "5b55082f30ebc731dc9ce1ede8e652fa",
+    "url": "/kids-typing/static/js/2.eeb8d9ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ccd65896cfffc35565c3",
+    "url": "/kids-typing/static/js/main.2f4d59ba.chunk.js"
+  },
+  {
+    "revision": "41bc5cca450d10d2bfd2",
+    "url": "/kids-typing/static/js/runtime-main.2efd3920.js"
+  },
+  {
+    "revision": "adf9ff13c449200ef765a82c50781009",
+    "url": "/kids-typing/static/media/background-ending.adf9ff13.jpg"
+  },
+  {
+    "revision": "3d93655f14fcea438b582170bbbb1620",
+    "url": "/kids-typing/static/media/background-goldfish.3d93655f.jpg"
+  },
+  {
+    "revision": "df1ad9ba8c065274247bed435c423f21",
+    "url": "/kids-typing/static/media/background-halloween.df1ad9ba.png"
+  },
+  {
+    "revision": "12ff595a7bcada98b0c3a02d887a19ec",
+    "url": "/kids-typing/static/media/background-outer-frame.12ff595a.png"
+  },
+  {
+    "revision": "9f336ef6d1bf3e6ef0afeea42b1512bf",
+    "url": "/kids-typing/static/media/background-sakura.9f336ef6.png"
+  },
+  {
+    "revision": "8b75e003731edf07f45082f45d752ff1",
+    "url": "/kids-typing/static/media/background-seabed.8b75e003.png"
+  },
+  {
+    "revision": "6bc2576e2d7b3dc783bc211fa15a951e",
+    "url": "/kids-typing/static/media/background-sunflower.6bc2576e.png"
+  },
+  {
+    "revision": "8a280a7e9c257e73e14957e46ecfed6b",
+    "url": "/kids-typing/static/media/background-title.8a280a7e.png"
+  },
+  {
+    "revision": "c4575386b948aea697a9c0d54dc0f523",
+    "url": "/kids-typing/static/media/bgm-ending@chocolatedaisakusen.c4575386.mp3"
+  },
+  {
+    "revision": "d1b48e24ff07478099cadc9c8b294155",
+    "url": "/kids-typing/static/media/bgm-stage-end@nichinichikorekoujitsu.d1b48e24.mp3"
+  },
+  {
+    "revision": "77ab81b7b863ba6d53a86ae54902fd0e",
+    "url": "/kids-typing/static/media/bgm-stage-goldfish@retropark.77ab81b7.mp3"
+  },
+  {
+    "revision": "6d81b79eb0cb9db837ad064ce5086813",
+    "url": "/kids-typing/static/media/bgm-stage-halloween@sunadokeiseiun.6d81b79e.mp3"
+  },
+  {
+    "revision": "99ca7ff61b147c655bf62d04dc1a2773",
+    "url": "/kids-typing/static/media/bgm-stage-sakura@sanjinooyatsu.99ca7ff6.mp3"
+  },
+  {
+    "revision": "d2f8d4e0f56a426ba2a2d2af48016433",
+    "url": "/kids-typing/static/media/bgm-stage-seabed@retrogamecenter2.d2f8d4e0.mp3"
+  },
+  {
+    "revision": "0d1f4fdac622e002c9fb80d687586b0b",
+    "url": "/kids-typing/static/media/bgm-stage-sunflower@lovelyflower.0d1f4fda.mp3"
+  },
+  {
+    "revision": "c45367d2c2606ec7fdcbaf6231d69c1b",
+    "url": "/kids-typing/static/media/bgm-title@happytime.c45367d2.mp3"
+  },
+  {
+    "revision": "c31133a0681d6278469f067de6c0e658",
+    "url": "/kids-typing/static/media/character-bird.c31133a0.png"
+  },
+  {
+    "revision": "eb4e1f3250b8f79769c7424a46760414",
+    "url": "/kids-typing/static/media/character-blackfish.eb4e1f32.png"
+  },
+  {
+    "revision": "86ffc1473dfb0da1c1cab93696522464",
+    "url": "/kids-typing/static/media/character-blowfish.86ffc147.png"
+  },
+  {
+    "revision": "6f416b192d67803a2b198b0dd358eb97",
+    "url": "/kids-typing/static/media/character-catfish.6f416b19.png"
+  },
+  {
+    "revision": "e6af5909df4e53ca6e61ffd9e9a21d49",
+    "url": "/kids-typing/static/media/character-dragonfly.e6af5909.png"
+  },
+  {
+    "revision": "752661e8d34829d84f4cb9f041633a45",
+    "url": "/kids-typing/static/media/character-goldfish.752661e8.png"
+  },
+  {
+    "revision": "c4204330bce80d41d7aed440d47a1e93",
+    "url": "/kids-typing/static/media/character-ladybird.c4204330.png"
+  },
+  {
+    "revision": "79df4899398a49c3ef8727cb177737d7",
+    "url": "/kids-typing/static/media/character-octopus.79df4899.png"
+  },
+  {
+    "revision": "650bf64d168772edc7e1ebb7b6c1fa3c",
+    "url": "/kids-typing/static/media/character-pumpkin.650bf64d.png"
+  },
+  {
+    "revision": "3ba0f8cb6320bdf19414970dfd9944fc",
+    "url": "/kids-typing/static/media/character-shark.3ba0f8cb.png"
+  },
+  {
+    "revision": "536a0bf182ab44948822995371853a03",
+    "url": "/kids-typing/static/media/character-turtle.536a0bf1.png"
+  },
+  {
+    "revision": "c2d0d37e7a8300fce1e729ea82a9c1c8",
+    "url": "/kids-typing/static/media/character-witch.c2d0d37e.png"
+  },
+  {
+    "revision": "81b58c07d4a8008e1fe561fa9d3fd635",
+    "url": "/kids-typing/static/media/practice-end-level-up.81b58c07.png"
+  },
+  {
+    "revision": "ffef03c67bbdd5ee639de3472b33f5ee",
+    "url": "/kids-typing/static/media/se-fire-character@jump13.ffef03c6.mp3"
+  },
+  {
+    "revision": "8bf494ca8a911240f5f53e0f6534f8b0",
+    "url": "/kids-typing/static/media/se-miss.8bf494ca.mp3"
+  },
+  {
+    "revision": "40e914b9986208a9a6ba77a0f0c5c73f",
+    "url": "/kids-typing/static/media/se-stage-sakura-start@rappa.40e914b9.mp3"
+  },
+  {
+    "revision": "75de09cc595dd892f821c95446a995dd",
+    "url": "/kids-typing/static/media/se-success.75de09cc.mp3"
+  },
+  {
+    "revision": "84ec48ebd5807771651c3d84292b4be5",
+    "url": "/kids-typing/static/media/se-title-start.84ec48eb.mp3"
+  },
+  {
+    "revision": "c61cc0ef3204013e1c4a8ec72cffe461",
+    "url": "/kids-typing/static/media/se-voice-congratulation.c61cc0ef.mp3"
+  },
+  {
+    "revision": "ce00be93c36139dcecfb68b936ac31cc",
+    "url": "/kids-typing/static/media/se-voice-count-down.ce00be93.mp3"
+  },
+  {
+    "revision": "7301e778b6c59f0945aeb68ac9ab5609",
+    "url": "/kids-typing/static/media/se-voice-go.7301e778.mp3"
+  },
+  {
+    "revision": "b0b324a0e2e9473192e1aecfa707f9b0",
+    "url": "/kids-typing/static/media/se-voice-good-job.b0b324a0.mp3"
+  },
+  {
+    "revision": "c92346f83511fe8e2d9fa8cc6fc6a9ac",
+    "url": "/kids-typing/static/media/se-voice-pity.c92346f8.mp3"
+  },
+  {
+    "revision": "e4c431288a2658fdc1f95a7b1fed70ab",
+    "url": "/kids-typing/static/media/se-voice-ready.e4c43128.mp3"
+  },
+  {
+    "revision": "7fac2dbf9e7197663bfc984a34e568f1",
+    "url": "/kids-typing/static/media/se-voice-soon-after.7fac2dbf.mp3"
+  }
+]);

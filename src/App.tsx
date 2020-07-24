8f5f6365a3189ebd666e682f50b30977b307d71f
manabee99 <mab@ss.iij4u.or.jp>
2020-07-24 14:04:28 +0900

    update git pages で公開するための定義などを追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import React from "react";
-import PracticePageContainer from "./containers/PracticePageContainer";
-import TitlePageContainer from "./containers/TitlePageContainer";
-import { Route, Switch } from "react-router";
-import { BrowserRouter } from "react-router-dom";
+import React from 'react';
+import PracticePageContainer from './containers/PracticePageContainer';
+import TitlePageContainer from './containers/TitlePageContainer';
+import { Route, Switch, Redirect } from 'react-router';
+import { BrowserRouter } from 'react-router-dom';
 import EndingPageContainer from './containers/EndingPageContainer';
 
 const App: React.FC = (props) => {
@@ -10,25 +10,16 @@ const App: React.FC = (props) => {
     <React.Fragment>
       <BrowserRouter>
         <Switch>
-
           {/* タイトルページ（練習後に戻った時）*/}
-          <Route
-            path="/kids-typing/title/:soundMuting"
-            component={TitlePageContainer}
-          ></Route>
+          <Route path='/kids-typing/title/:soundMuting' component={TitlePageContainer}></Route>
 
           {/* 練習ページ */}
-          <Route
-            path="/kids-typing/practice"
-            component={PracticePageContainer}
-          ></Route>
+          <Route path='/kids-typing/practice' component={PracticePageContainer}></Route>
 
           {/* エンディングページ */}
-          <Route
-            path="/kids-typing/ending/:soundMuting"
-            component={EndingPageContainer}
-          ></Route>
+          <Route path='/kids-typing/ending/:soundMuting' component={EndingPageContainer}></Route>
 
+          <Redirect to='/kids-typing/title/false' />
         </Switch>
       </BrowserRouter>
     </React.Fragment>
@@ -36,4 +27,3 @@ const App: React.FC = (props) => {
 };
 
 export default App;
-
